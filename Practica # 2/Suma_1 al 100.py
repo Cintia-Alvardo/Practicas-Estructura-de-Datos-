b0bd99873e69f2e38bcdbf9997d2e4005c642d12
Cintia-Alvardo <140437056+Cintia-Alvardo@users.noreply.github.com>
2023-08-19 04:28:08 -0800

    Add files via upload

---
+++ Practica # 2/Suma_1 al 100.py
@@ -0,0 +1,6 @@
+num = 1
+suma = 0
+while num <= 100:
+    suma = suma + num 
+    num += 1
+print("La suma de los números del 1 al 100 es: ", suma)
---
+++ Practica # 2/contar_vocales.py
@@ -0,0 +1,6 @@
+palabras = "Hola estoy programando en python"
+
+vocales = ['a', 'e', 'i', 'o', 'u']
+
+for vocal in vocales:
+    print (vocal, palabras.count(vocal))
