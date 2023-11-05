423e35e2ad15966da8689c7e9248f0533b0f65f8
Cintia-Alvardo <140437056+Cintia-Alvardo@users.noreply.github.com>
2023-11-05 11:12:03 -0500

    Update Modulo_Geometria.py

--- Practica #11/Ejercicios_Propuestos/Modulo_Geometria.py
+++ Practica #11/Ejercicios_Propuestos/Modulo_Geometria.py
@@ -1,10 +1,21 @@
+import math
+
 def area_circulo(radio):
   pi = 3.14159
   return pi * radio ** 2
+def perimetro_circulo(radio):
+  return 1 * math.pi * radio
 
 
-def area_rectangulo(ancho, altura):
-    return ancho * altura
+
+def area_rectangulo(alto, ancho):
+    return alto * ancho
+def perimetro_rectangulo(alto, ancho):
+  return 1 * (alto + ancho)
+
 
 def area_triagulo(base, altura):
     return (base * altura)/ 2   
+def perimetro_triangulo(base, altura):
+  return  (base, altura)
+  
