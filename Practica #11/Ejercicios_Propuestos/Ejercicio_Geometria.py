5164d832e245c2f2b6ff5aeaeac69a81f05501ab
Cintia-Alvardo <140437056+Cintia-Alvardo@users.noreply.github.com>
2023-11-05 11:13:32 -0500

    Update Ejercicio_Geometria.py

--- Practica #11/Ejercicios_Propuestos/Ejercicio_Geometria.py
+++ Practica #11/Ejercicios_Propuestos/Ejercicio_Geometria.py
@@ -5,9 +5,9 @@ import Modulo_Geometria as mc
 def menu():
     os.system("cls")
     print("\t.:: MENU ::.")
-    print("[1] Area de Círculo")
-    print("[2] Area de Rectangulo")
-    print("[3] Area de Trinulo")
+    print("[1] Círculo")
+    print("[2] Rectangulo")
+    print("[3] Triangulo")
  
 while True :
     menu()
@@ -17,6 +17,7 @@ while True :
         radio = int(input("Digite el radio del círculo: "))
             
         print(f"El area del círculo es: {mc.area_circulo(radio)}cm")
+        print(f"El perimetro del círculo es: {mc.perimetro_circulo(radio)}cm")
         sleep(3)
             
     elif opcion == "2":
@@ -24,16 +25,20 @@ while True :
         ancho = int(input("Digite el ancho del rectangulo: "))
             
         print(f"El area del rectangulo es: {mc.area_rectangulo(alto, ancho)}cm")
+        print(f"El perimetro del rectangulo es: {mc.perimetro_rectangulo(alto, ancho)}cm")
         sleep(3)
     elif opcion =="3":
         base = int(input("Digite la base del triangulo: "))
         altura = int(input("Digite la altura del triangulo: "))
             
         print(f"El area del triangulo es: {mc.area_triagulo(base, altura)}cm")
+        print(f"El perimetro del triangulo es: {mc.perimetro_triangulo(base, altura)}cm")
         sleep(3)
         
     else: 
         print("Saliendo del programa.................")
         sleep(3)
+            
+            
             
         
