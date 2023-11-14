4cefc6d278151bcd590e6cdaa1d71c8b9addea2c
Cintia-Alvardo <140437056+Cintia-Alvardo@users.noreply.github.com>
2023-11-13 20:28:19 -0500

    Update Ejercicio_invertir_cadena.py

--- Practica # 12/Ejercicio_invertir_cadena.py
+++ Practica # 12/Ejercicio_invertir_cadena.py
@@ -1,11 +1,19 @@
+class CadenaVacia(Exception):
+    def _init_(self):
+        super()._init_("La cadena está vacía")
+
 def invertir_cadena(cadena):
     if len(cadena) <= 1:
         return cadena
-    
     else:
         return cadena[-1] + invertir_cadena(cadena[1:-1]) + cadena[0]
 
-cadena_original = "Cintia Alvarado"
-cadena_invertida = invertir_cadena(cadena_original)
-print(f"Cadena original: {cadena_original}")
-print(f"Cadena invertida: {cadena_invertida}")
+try:
+    cadena_original = "Cintia Alvarado"
+    if len(cadena_original) == 0:
+        raise CadenaVacia()
+    cadena_invertida = invertir_cadena(cadena_original)
+    print(f"Cadena original: {cadena_original}")
+    print(f"Cadena invertida: {cadena_invertida}")
+except CadenaVacia as e:
+    print(e)
