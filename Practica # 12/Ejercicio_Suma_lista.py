dee64055b97b38f1b2d6178a24cf06687acbee2d
Cintia-Alvardo <140437056+Cintia-Alvardo@users.noreply.github.com>
2023-11-13 20:27:41 -0500

    Update Ejercicio_Suma_lista.py

--- Practica # 12/Ejercicio_Suma_lista.py
+++ Practica # 12/Ejercicio_Suma_lista.py
@@ -1,11 +1,20 @@
-def suma_lista(lista):
-    
-    if not lista:
-        return 0
-    else:
-        return lista[0] + suma_lista(lista[1:])
+class NoEsListaError(Exception):
+   def init(self, mensaje):
+       self.mensaje = mensaje
+       super().init(self.mensaje)
 
+def suma_lista(lista):
+   if not (lista, list):
+       raise NoEsListaError("La entrada no es una lista")
+   elif not lista:
+       return 0
+   else:
+       return lista[0] + suma_lista(lista[1:])
 
 mi_lista = [40, 7, 29, 13, 19]
-resultado = suma_lista(mi_lista)
-print(f"La suma de los números de la lista {mi_lista} es: {resultado}")
+try:
+   resultado = suma_lista(mi_lista)
+   print(f"La suma de los numeros de la lista es: {resultado}")
+except NoEsListaError as e:
+ print(e)
+
